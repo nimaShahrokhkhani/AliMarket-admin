f599a034dbb1ad448abf3ad25b98c9213a0877fa
Nima Shahrokhkhani <nima.shahrokhkhani@gmail.com>
2023-06-07 12:03:41 -0400

    Initial Commit

---
+++ AliMarket/AliMarketApp.swift
@@ -0,0 +1,17 @@
+//
+//  AliMarketApp.swift
+//  AliMarket
+//
+//  Created by Nima on 6/7/23.
+//
+
+import SwiftUI
+
+@main
+struct AliMarketApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
